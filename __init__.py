0d8110057366edfeb2256a3234ee03262d09d0d5
Olivier Everts <oliviereverts98@gmail.com>
2021-01-08 14:07:55 -0500

    Made a lot of changes to the skill

--- __init__.py
+++ __init__.py
@@ -10,30 +10,40 @@ class MyFirstSkill(MycroftSkill):
         
     @intent_handler('skill.study.intent')
     def handle_skill_study(self, message):
+        
+        # Get the amount of blocks from the user
         blocks = self.get_response('skill.study')
-        self.speak_dialog('skill.study.confirmation', {'time': str(blocks)})
-# To convert  blocks to an int, itt first needs to be a  string. The variable must be of time int for us to be able to put it into a range for the  loop
+        
+        # To convert  blocks to an int, it first needs to be a string. 
+        # The variable must be of type int for further use
         blocks = str(blocks)
         blocks = int(blocks)
         
+        # If the user selects one block, Mycroft responds with the singular "block" 
+        if blocks == 1:
+            amount_of_blocks = "1 block"
+        
+        # If the user selects multiple blocks, Mycroft responds with the plural "blocks"
+        if blocks > 1:
+            amount_of_blocks = "{} blocks".format(str(blocks))
+            
+        # Get the current time to calculate the time when the user will be done in the future
         now = datetime.now()
         currenttime = now.strftime("%H:%M")
         
-        if blocks == 1:
-            minutes = 25
-            futuretime = now + timedelta(minutes = minutes)
-            futuretime = futuretime.strftime("%H:%M")
-            self.speak_dialog('skill.studytime', data={'currenttime': currenttime,
-                                                         'blocks': str(blocks),
-                                                         'futuretime': futuretime})
-        if blocks > 1:
-            minutes = blocks*25 + (blocks-1)*5
-            futuretime = now + timedelta(minutes = minutes)
-            futuretime = futuretime.strftime("%H:%M")
-            self.speak_dialog('skill.studytime', data={'currenttime': currenttime,
-                                                         'blocks': str(blocks),
-                                                         'futuretime': futuretime})
+        # Calculate the time when the user is done based on the amount of blocks selected
+        # Each block is 25 minutes and each break is 5 minutes
         
+        minutes = blocks*25 + (blocks-1)*5
+        futuretime = now + timedelta(minutes = minutes)
+        futuretime = futuretime.strftime("%H:%M")
+        
+        # Respond to the user with the amount of blocks they have chosen, what the curren time is
+        # and when their study session will be finished
+        self.speak_dialog('skill.study.confirmation', data={'currenttime': currenttime,
+                                                            'amount_of_blocks': amount_of_blocks,
+                                                            'futuretime': futuretime})
+
 #The time.sleep() function  takes an input of seconds. So if  you want the code to sleep for several minutes you should multiply the values  witth  60
 #Currently everythting is in second for debugging purposes
         for i in range(blocks):
